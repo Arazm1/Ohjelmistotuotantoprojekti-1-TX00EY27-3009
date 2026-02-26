23297e02a8068e4d46e5e1ab03ab8e7267b8c8d5
Arazm1 <araz.muhamm3d@gmail.com>
2026-02-26 14:58:25 +0200

    Updated TemperatureConverter to have the main class.

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
 
-    public double fahrenheitToCelsius(double inputFahrenheit){
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the fahrenheit to be converted into Celsius: ");
+        double inputFahrenheit = scanner.nextDouble();
+        System.out.println(fahrenheitToCelsius(inputFahrenheit));
+    }
+
+
+
+    public static double fahrenheitToCelsius(double inputFahrenheit){
         double resultInCelsius = (inputFahrenheit - 32) * 5 / 9;
         return resultInCelsius;
     }
 
-    public double celsiusToFahrenheit(double inputCelsius){
+    public static double celsiusToFahrenheit(double inputCelsius){
         double resultInFahrenheit = (inputCelsius * 9 / 5 + 32);
         return  resultInFahrenheit;
     }
 
-    public boolean isExtremeTemperature(double inputCelsius){
+    public static boolean isExtremeTemperature(double inputCelsius){
         if(inputCelsius < -40 || inputCelsius>50){
             return true;
         }
@@ -19,7 +31,7 @@ public class TemperatureConverter {
         }
     }
 
-    public double kelvinToCelsius(double inputKelvin){
+    public static double kelvinToCelsius(double inputKelvin){
         return inputKelvin - 273.15;
     }
 }
