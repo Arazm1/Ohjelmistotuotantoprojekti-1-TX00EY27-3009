13800614ca9b70ccea231055e3d8376918547da8
Arazm1 <araz.muhamm3d@gmail.com>
2026-04-03 15:10:12 +0300

    Adjustments.

--- src/main/java/fuelcon/datasource/MariaDbConnection.java
+++ src/main/java/fuelcon/datasource/MariaDbConnection.java
@@ -17,10 +17,9 @@ public class MariaDbConnection {
     private static final String USER = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PASSWORD");
      */
-    private static final String URL = "jdbc:mysql://localhost:3306/sep2_week3hw";
-    private static final String USER = "user4";
-    private static final String PASSWORD = "password";
-
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL + "?user=" + USER + "&password=" + PASSWORD);
